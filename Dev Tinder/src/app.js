4c4dffcb3342f5f97a7ae061f0244e99c8b0207f
Suraj Kumar <20203048.it@gmail.com>
2025-01-17 12:35:28 +0530

    exploring get, post, delete

--- Dev Tinder/src/app.js
+++ Dev Tinder/src/app.js
@@ -4,7 +4,18 @@
  // we r sending incoming request to the server
  // it is responce handelar
  
- 
+ app.use("/user",(req, res, next)=>{
+        next();
+      // res.send("handling the route user 1")
+},
+(req, res, next)=>{
+        next();
+      // res.send("handling the route user 2")
+},
+(req, res, next)=>{
+      res.send("handling the route user 3")
+}
+)
  
 app.get("/user", (req, res)=>{
          res.send({firstname:"suraj", lastname:"kumar"})
@@ -16,9 +27,7 @@ app.delete("/user", (req, res)=>{
          res.send("data deleted sucessfully");
 })
 // if i put this on top then it will show for all http request
-app.use("/user",(req, res)=>{
-      res.send("hello my love where r u")
-})
+ 
 
  // this is the port where application will run
  app.listen(3000, ()=>{
