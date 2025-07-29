1f247827a3a7a48b742371934a03c6d4a75ad73f
Suraj Kumar <20203048.it@gmail.com>
2025-07-29 18:16:49 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -1,39 +1,13 @@
- const express=require("express");
- const app=express();
+const express=require("express");
+const app=express();
 
- // we r sending incoming request to the server
- // it is responce handelar
- 
- app.use("/user",(req, res, next)=>{
-        next();
-      // res.send("handling the route user 1")
-},
-(req, res, next)=>{
-        next();
-      // res.send("handling the route user 2")
-},
-(req, res, next)=>{
-      // res.send("handling the route user 3")
-        next();
-},
-(req, res, next)=>{
-      res.send("handling the route user 3")
-}
-)
- 
-app.get("/user", (req, res)=>{
-         res.send({firstname:"suraj", lastname:"kumar"})
+app.use("/test",(req, res)=>{
+      res.end("hello i m from backend side")
 })
-app.post("/user", (req, res)=>{
-        res.send("data sucessfully save to the database")
+app.use("/hello",(req, res)=>{
+      res.end("hello how r u")
 })
-app.delete("/user", (req, res)=>{
-         res.send("data deleted sucessfully");
-})
-// if i put this on top then it will show for all http request
- 
 
- // this is the port where application will run
- app.listen(3000, ()=>{
-       console.log("server is hosted on port 3000")
- });
+app.listen(3000, () => {
+      console.log("my server is running on port on 3000....")
+});
